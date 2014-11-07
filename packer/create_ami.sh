2bc6d6f601cf9bf40f274cd5be2dad199575dded
Corey Ramsden <corey.ramsden@gmail.com>
2014-11-07 02:21:40 -0500

    changed directory for packer

--- packer/create_ami.sh
+++ packer/create_ami.sh
@@ -5,7 +5,7 @@ MY_BUILD_DIR=$1/build/dist; export MY_BUILD_DIR
 MY_BUILD_FILE=$2-$3.tar.gz; export MY_BUILD_FILE
 MY_BUILD=$MY_BUILD_DIR/$MY_BUILD_FILE; export MY_BUILD
 echo "This is the build: " $MY_BUILD
-cd /home/packer
+cd /home/provisioning/packer
 echo "Launching packer to create webserver AMI"
 /usr/local/packer/packer build -var-file=variables.json webserver-template.json 
 
